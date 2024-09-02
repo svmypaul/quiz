d0e3b97321e1581e04529cdbdd0818cca4042925
svmypaul <svmypaul024@gmail.com>
2024-09-02 18:53:30 +0530

    v2
    added audio player of each words

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from streamlit_js_eval import streamlit_js_eval
 import random
 import pandas as pd
+from gtts import gTTS
+import io
 
 def load_questions(file):
     df = pd.read_excel(file, engine='openpyxl')
@@ -9,23 +11,18 @@ def load_questions(file):
     random.shuffle(questions)
     return questions
 
+def text_speech(text):
+    myobj = gTTS(text=text, lang='en', slow=True)  ## converting audio format
+    audio_bytes = io.BytesIO()
+    myobj.write_to_fp(audio_bytes)
+    audio_bytes.seek(0)
+    return audio_bytes
+
+
+
 file = st.file_uploader('upload your file')
 
-# Define questions and answers
-# questions = [
-#     {
-#         "question": "What is the capital of France?",
-#         "answer": "Paris"
-#     },
-#     {
-#         "question": "What is 2 + 2?",
-#         "answer": "4"
-#     },
-#     {
-#         "question": "What is the largest ocean on Earth?",
-#         "answer": "Pacific"
-#     }
-# ]
+
 startBtn = st.button('Start')
 
         
@@ -41,12 +38,20 @@ if 'quiz_started' in st.session_state and st.session_state.quiz_started:
     questions = st.session_state.questions
     
     for current_question in range(len(questions)):
+        col1, col2 = st.columns(2)
     # Display the current question
-        st.text(questions[current_question]["question"])
-
+        with col1:
+            st.markdown(f'<p style="color:red;">{questions[current_question]["question"]}</p>', unsafe_allow_html=True)
+        with col2:
+            st.audio(text_speech(questions[current_question]["question"]), format='audio/mp3')
         # Button to view the answer
         if st.button('View Answer', key=f'view_{current_question}'):
-            st.info(questions[current_question]["answer"])
+            col1, col2 = st.columns(2)
+            st.markdown("---")
+            with col1:
+                st.success(questions[current_question]["answer"])
+            with col2:
+                st.audio(text_speech(questions[current_question]["answer"]), format='audio/mp3')  # playing audio
 
 
     if st.button('Reload'):
